1622693cf9cb23f610a465174e2e46c2d6c2b872
Brett Morris <brettmorris21@gmail.com>
2017-10-19 19:17:25 -0700

    simplifying setup.py

--- setup.py
+++ setup.py
@@ -1,57 +1,6 @@
 from setuptools import setup
-import os
-import sys
-from setuptools.command.install import install
-import subprocess
 
 packagename = 'rms'
-
-
-def get_virtualenv_path():
-    """Used to work out path to install compiled binaries to."""
-    if hasattr(sys, 'real_prefix'):
-        return sys.prefix
-
-    if hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix:
-        return sys.prefix
-
-    if 'conda' in sys.prefix:
-        return sys.prefix
-
-    return None
-
-
-def compile_and_install_software():
-    """
-    Used the subprocess module to compile/install the C software.
-
-    Return the path of the new STSP executable, and the path we want to
-    move it to when the installation is complete.
-    """
-    src_path = './STSP/'
-
-    # compile the software
-    cmd = "gcc -lm stsp.c -o stsp_rms"
-    venv = get_virtualenv_path()
-    if venv:
-        cmd += ' --prefix=' + os.path.abspath(venv)
-    subprocess.check_call(cmd, cwd=src_path, shell=True)
-    pkg_dir = os.path.join(sys.exec_prefix, 'lib',
-                           "python{0}.{1}".format(sys.version_info.major,
-                                                  sys.version_info.minor),
-                           'site-packages', packagename)
-    executable_build_path = os.path.join(src_path, 'stsp_rms')
-    executable_new_path = os.path.join(pkg_dir, 'stsp_rms')
-    return executable_build_path, executable_new_path
-
-
-class CustomInstall(install):
-    """Custom handler for the 'install' command."""
-    def run(self):
-        #paths = compile_and_install_software()
-        super(CustomInstall, self).run()
-        #os.rename(*paths)
-
 
 setup(name=packagename,
       version='0.1',
@@ -66,5 +15,4 @@ setup(name=packagename,
       packages=[packagename],
       include_package_data=True,
       package_data={"": ["LICENSE"]},
-      cmdclass={'install': CustomInstall}
 )
